d2f3854b13029a5298b7b23b56019bbd82b0a07b
cole <coleheylen12@gmail.com>
2022-05-31 11:16:52 +0200

    Updated util tests

--- api/tests/test_utils.py
+++ api/tests/test_utils.py
@@ -1,19 +1,11 @@
-import email
-from email.mime import application
-from urllib import response
-from django.test import TestCase
-from requests import request
-from rest_framework.test import APITestCase, APIClient, APIRequestFactory, force_authenticate
-from api.models import Product, Catalogue
+from rest_framework.test import APITestCase, APIClient, force_authenticate
 from django.contrib.auth.models import User
 from rest_framework import status
-from django.urls import reverse
 from rest_framework_jwt import utils
 from rest_framework_jwt.settings import api_settings
 from api.utils import jwt_decode_token, jwt_get_username_from_payload_handler
 import jwt
 from cryptography.hazmat.primitives import serialization
-import requests
 
 class TestUtils(APITestCase):
 
@@ -22,12 +14,21 @@ class TestUtils(APITestCase):
         self.user = User.objects.create_user('test', 'test@test.com', 'abc123')
 
     def test_should_check_jwt_get_username_from_payload_handler(self):
-
         user = '12345'
         payload = {'sub':'12345', 'email':'test@test.com', 'is_staff':False}
         username = jwt_get_username_from_payload_handler(payload)
         self.assertEqual(username, user)
-
+"""
     def test_should_check_jwt_decode_token(self):# try include uuid with refactor
-        pass
+        headers = {"typ": "JWT", "alg": "RS256", "kid":"JhutschiIutrvjGrgx"}
+        payload = {"sub": "127853487354", "name": "test"}
 
+        private_key = open('.ssh/id_rsa', 'r').read()
+        
+        key = serialization.load_ssh_private_key(private_key.encode(), password=b'')
+
+        token = jwt.encode(payload=payload, key=key, algorithm='RS256', headers=headers)
+        #print(token, '\n')
+        util_func = jwt_decode_token(token)
+
+"""
